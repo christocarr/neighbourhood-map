d19106afb0ca5a762f6904211a7e22af5641895a
Chris Carr <christocarr@outlook.comgit config --global user.email christocarr@outlook.comgit config --global user.email christocarr@outlook.com>
2018-07-29 13:01:24 +0100

    Add MapContainer component

---
+++ src/MapContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class MapContainer extends Component {
+  state = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 10
+  }
+
+  render() {
+
+    console.log()
+
+    return (
+      <div style={{height: '100vh', width: '100vw'}}>
+       
+      </div>  
+    )
+  }  
+}
+
+export default MapContainer
