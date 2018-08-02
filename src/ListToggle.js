c09dc0834e1ff9771447631a5021f523af657407
Chris Carr <christocarr@outlook.com>
2018-08-02 21:10:12 +0100

    Change MarkerListOpener to MarkerList and add component ListToggle as child to MarkerList

---
+++ src/ListToggle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListToggle = () => {
+  return (
+    <div className='list-toggle'>
+      > {/*to be replaced by a logo*/}
+    </div>
+  )
+}
+
+export default ListToggle
