08b7517eee1c594be92906d134f3dacaa0e6b624
Chris Carr <christocarr@outlook.com>
2018-08-02 20:03:25 +0100

    Add component that opens marker list and filter

---
+++ src/MarkerListOpener.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MarkerListOpener = () => {
+  return (
+  <div className='marker-list'>
+    > {/*to be replaced by a logo*/}
+  </div>  
+  )
+}
+export default MarkerListOpener
