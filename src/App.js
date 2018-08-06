5ef44aeeca7c4986a4d90d2edb4aaa1b8226d75e
Chris Carr <christocarr@outlook.com>
2018-08-06 20:23:24 +0100

    Add fetch Foursquare API in componentDidMount

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ import './App.css';
 class App extends Component {
 
   state = {
-    markerListOpen: false
+    markerListOpen: false,
+    listLocations: null,
+  }
+
+  componentDidMount() {
+    fetch('https://api.foursquare.com/v2/venues/search?ll=51.5055,-0.0754&query=parking&limit=6&radius=2000&client_id=5MFR2FIONG3CCN5IXQBUE3HFBKEECHZZ0Q1XNZVPZQEZMZDQ&client_secret=XVR2PAUVWZSPQY5RY0HZV54ZMBBXM2CWG4RKCGGQUT0JC0OU&v=20181001')
+      .then(res => res.json())
+      .then(data => this.setState({ listLocations: data }))
   }
 
   handleToggle = () => {
@@ -20,10 +27,11 @@ class App extends Component {
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className='app'>
         <Header />
-        <MapContainer />
+        <MapContainer defaultLocation={this.state.defaultLocation}/>
         <MarkerList 
           markerListOpen={this.state.markerListOpen}
           toggleList={this.handleToggle}
@@ -36,4 +44,6 @@ class App extends Component {
 
 export default App
 
-// Maps API key: AIzaSyCIXjUR2aOOj9EGjnJVcG9pi3294fTp8yI
+// Google Maps API key: AIzaSyCIXjUR2aOOj9EGjnJVcG9pi3294fTp8yI
+//Foursquare Client ID: 5MFR2FIONG3CCN5IXQBUE3HFBKEECHZZ0Q1XNZVPZQEZMZDQ
+//Foursquare Client Secret: XVR2PAUVWZSPQY5RY0HZV54ZMBBXM2CWG4RKCGGQUT0JC0OU
