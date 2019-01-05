4cd5dbbacf3cdd2fa7265afda659b8a65c8f9a32
christocarr <christocarr@outlook.com>
2019-01-05 11:08:50 +0000

    Updates

---
+++ precache-manifest.bcb88a131b97f6c413363426d32b91d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1181d8d61333a87223a5bd0c37856226",
+    "url": "/neighbourhood-map/static/media/menu.1181d8d6.svg"
+  },
+  {
+    "revision": "b4f46611e166d396c4e4",
+    "url": "/neighbourhood-map/static/js/runtime~main.b4f46611.js"
+  },
+  {
+    "revision": "ab21249317cb8cca543b",
+    "url": "/neighbourhood-map/static/js/main.ab212493.chunk.js"
+  },
+  {
+    "revision": "ef7a01c46d02f02802f7",
+    "url": "/neighbourhood-map/static/js/1.ef7a01c4.chunk.js"
+  },
+  {
+    "revision": "ab21249317cb8cca543b",
+    "url": "/neighbourhood-map/static/css/main.c9d2a9af.chunk.css"
+  },
+  {
+    "revision": "b8179c6972772f8c8b0060541b15f2bc",
+    "url": "/neighbourhood-map/index.html"
+  }
+];
